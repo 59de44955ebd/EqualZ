d4f6e8b937b41919b7bbdd62852548385c0cd55a
59de44955ebd <59de44955ebd@gmail.com>
2024-12-02 16:25:09 +0100

    Some cleanup

--- README.md
+++ README.md
@@ -17,7 +17,8 @@ It instead uses bundled versions of [Ziamath](https://github.com/cdelker/ziamath
 ![EqualZ in Windows 131](screenshots/equalz_win11.png)
 
 ## Notes
-* To keep the file size and memory consumption of the frozen .exe small, it uses a slightly adjusted private copy of CairoSVG (renamed to "cairosvg_min") that has the dependancy on Pillow (which in turn would depend on Numpy) removed - CairoSVG uses Pillow only for a feature that EqualZ doesn't need anyway. But EqualZ works perfectly fine also with the original unaltered version of CairoSVG, just change the import at the top of main.py accordingly.
+* To keep the file size and memory consumption of the frozen .exe small, it uses a slightly adjusted local copy of CairoSVG (renamed to "cairosvg_min") that has the dependancy on Pillow (which in turn would depend on Numpy) removed - CairoSVG uses Pillow only for a feature that EqualZ doesn't need anyway. But EqualZ works perfectly fine also with the original unaltered version of CairoSVG, just change the import at the top of main.py accordingly.
+* The local copy of cairocffi (renamed to "cairocffi_min"), which CairoSVG depends on, only contains a single change that makes sure that the provided small static version of cairo.dll is used, instead of a MSYS2/mingw64 .dll called libcairo-2.dll (with lots of external dependancies) if you have the mingw64 bin directory in the system path (like me). This is again only to make the frozen app smaller and totally optional.
 * The portable version for Windows saves bookmarks and history in a local folder called "data", whereas the installer version creates and uses the folder `C:\Users\<Username>\.equalz`.  
 The app detects at runtime if it runs in portable or installer mode by looking for a file called "portable" next to the .exe.
 
--- renderlabel.py
+++ renderlabel.py
@@ -9,20 +9,18 @@ from PyQt5.QtWidgets import QLabel
 class RenderLabel(QLabel):
 
     ########################################
-    # @constructor
+    #
     ########################################
     def __init__ (self, parent=None):
         super().__init__(parent)
         self._width = 0
         self._height = 0
-        #self.setAutoFillBackground(True)
-        #self.setScaledContents(True)
 
     ########################################
     #
     ########################################
     def resizeEvent (self, event):
-        self._update_margins() # for pixmap only
+        self._update_margins()
         super().resizeEvent(event)
 
     ########################################
@@ -33,8 +31,7 @@ class RenderLabel(QLabel):
             super().setPixmap(QPixmap())
             self._width = 0
             return
-        self._width = pm.width()
-        self._height = pm.height()
+        self._width, self._height = pm.width(), pm.height()
         self._update_margins()
         super().setPixmap(pm)
 
@@ -44,19 +41,10 @@ class RenderLabel(QLabel):
     def _update_margins(self):
         if self._width <= 0 or self._height <= 0:
             return
-        w = self.width()
-        h = self.height()
+        w, h = self.width(), self.height()
         if w <= 0 or h <= 0:
             return
-
-#        if w * self._height > h * self._width:
-#            m = int((w - (self._width * h / self._height)) / 2)
-#            self.setContentsMargins(m, 0, m, 0)
-#        else:
-#            m = int((h - (self._height * w / self._width)) / 2)
-#            self.setContentsMargins(0, m, 0, m)
-
-        # just center, never scale
+        # center image in label
         mx = int((w - self._width) / 2) if w > self._width else 0
         my = int((h- self._height) / 2) if h > self._height else 0
         self.setContentsMargins(mx, my, mx, my)
@@ -66,4 +54,3 @@ class RenderLabel(QLabel):
     ########################################
     def minimumSizeHint(self):
         return QSize(0, 0)
-#        return QSize(self._width, self._height)
