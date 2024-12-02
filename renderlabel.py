36cd5f3f2eb92269e89dc0ca80e96031252051f4
59de44955ebd <59de44955ebd@gmail.com>
2024-12-02 18:55:19 +0100

    Minor UI edits

--- renderlabel.py
+++ renderlabel.py
@@ -47,7 +47,8 @@ class RenderLabel(QLabel):
         # center image in label
         mx = int((w - self._width) / 2) if w > self._width else 0
         my = int((h- self._height) / 2) if h > self._height else 0
-        self.setContentsMargins(mx, my, mx, my)
+        #self.setContentsMargins(mx, my, mx, my)
+        self.setStyleSheet(f'padding: {my} {mx}')
 
     ########################################
     #
